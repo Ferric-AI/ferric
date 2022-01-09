6dd6b0e7ecf06a960121b36d582cec9cc3ad92e3
Nimar Arora <nimar.arora@devrev.ai>
2022-01-08 23:50:17 -0800

    code coverage

--- ferric/tests/checks.rs
+++ ferric/tests/checks.rs
@@ -17,3 +17,20 @@ fn check_formatting() {
 
     assert!(output.status.success());
 }
+
+#[test]
+fn check_clippy() {
+    let output = Command::new("cargo")
+        .arg("clippy")
+        .arg("--workspace")
+        .arg("--all-targets")
+        .arg("--verbose")
+        .output()
+        .expect("failed to run clippy");
+
+    println!("status: {}", output.status);
+    println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+    println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+
+    assert!(output.status.success());
+}
